1023b7ece2f0675e13205cfadf1d67e3e196c1dd
Neelu Saraswatibhatla <neelu@sbneelu.com>
2022-02-05 20:55:36 +0000

    Not on mobile page done

--- src/pages/offline/Offline.jsx
+++ src/pages/offline/Offline.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logos/full-text-3.png';
 import noWifiIcon from './assets/no-wifi.png';
 
 const Offline = () => (
-    <div className="main">
+    <div className="offline-main">
         <div className="logo-container">
             <img className='logo' src={logo} alt="Stravian Logo" />
         </div>
