aa7255c96419fb28b0fe18c5dcca6feb8a2938ef
Alex Riddell-Webster <ahr38@cam.ac.uk>
2022-02-14 11:26:44 +0000

    getBirdName on API, used this on the hatch page.

--- src/pages/hatch/Hatch.jsx
+++ src/pages/hatch/Hatch.jsx
@@ -3,16 +3,17 @@ import unhatched from './assets/unhatched.png';
 import parthatched from './assets/parthatched.png';
 import hatched from './assets/hatched.png';
 import React, { useState } from 'react';
+import { getBirdname } from '../../utils/api';
+const Birdname = getBirdname();
 
 
-
-/* Ideas - change the graphics so they don't suck, change the bird name so that it's the actual name of your bird, image of the bird that isn't generic, perhaps use a picture of the eagle's eggs, in a nest, make the egg shake */
+/* Ideas - change the graphics so they don't suck, image of the bird that isn't generic, perhaps use a picture of the eagle's egg in a nest, make the egg shake, redirect to bird page afterwards? */
 const Hatch = () => {
     const [hatchCount, setHatchCount] = useState(0);
     return <div className="hatch-main">
         <div className="main-section">
             <div className="message-caption">
-                {hatchCount < 3 ? "Tap the egg to hatch your bird" : hatchCount > 10 ? "Meet your bird" : "Keep going!"}
+                {hatchCount < 3 ? "Tap the egg to hatch your bird" : hatchCount > 10 ? "Meet your bird, " + Birdname : "Keep going!"}
             </div>
             <div className="egg" onClick={() => setHatchCount(hatchCount + 1)}>
                 <img src={hatchCount < 5 ? unhatched : hatchCount > 10 ? hatched : parthatched} alt="Egg" />
