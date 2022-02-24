a6fbef69fee45ef4a2bc9376ef51c2afe4555aa9
Alex Riddell-Webster <ahr38@cam.ac.uk>
2022-02-24 14:57:42 +0000

    aaaaaaa

--- src/utils/login.js
+++ src/utils/login.js
@@ -3,14 +3,12 @@ import { getUserDetails } from "./api";
 const loggedIn = () => !!localStorage.getItem('username');
 const logout = () => localStorage.clear();
 const login = async (loginCode) => {
-    localStorage.setItem('username', 'test');
-    return true;
     const userDetails = await getUserDetails(loginCode);
     if (userDetails == null) return false;
     const { username, jwt } = userDetails;
+    localStorage.setItem('username', username);
     localStorage.setItem('jwt', jwt);
     return true;
 }
-
 
 export { loggedIn, logout, login };
