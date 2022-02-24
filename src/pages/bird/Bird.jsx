3c99a029bf4b38cca832355bd0da0af01e27919c
Alex Riddell-Webster <ahr38@cam.ac.uk>
2022-02-24 13:12:50 +0000

    map map time

--- src/pages/bird/Bird.jsx
+++ src/pages/bird/Bird.jsx
@@ -3,9 +3,14 @@ import { getBirdfact, getBirdname } from '../../utils/api';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
+import { icon } from "leaflet";
 const birdname = getBirdname();
 const fact = getBirdfact();
-const position = [51.505, -0.09]
+const position = [51.505, -0.09];
+const ICON = new icon({
+    iconUrl: require("./assets/icon.png"),
+    
+});
 
 const Bird = () => {
 
@@ -20,8 +25,8 @@ const Bird = () => {
             {fact}
         </div> 
         <div  className='map'>
-        <MapContainer  center={position} zoom={13} scrollWheelZoom={false} style={{ width: "300px", height: "300px" }} >
-            <Marker position={position}> </Marker>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ width: "300px", height: "300px" }} >
+            <Marker position={position} icon={ICON}> </Marker>
             <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
         </MapContainer>
         </div>
