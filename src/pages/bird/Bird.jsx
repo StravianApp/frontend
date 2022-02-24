0735d6e5f4fba3030f074e63deb68e9365f21b72
Alex Riddell-Webster <ahr38@cam.ac.uk>
2022-02-24 12:39:02 +0000

    bird page

--- src/pages/bird/Bird.jsx
+++ src/pages/bird/Bird.jsx
@@ -1,19 +1,30 @@
 import "./bird.scss";
 import { getBirdfact, getBirdname } from '../../utils/api';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
 const birdname = getBirdname();
 const fact = getBirdfact();
+const position = [51.505, -0.09]
 
 const Bird = () => {
+
     return <div className='settwings-main'>
         <div className='title'>
             {birdname}
         </div>
-        <div className='subtitle'>
-            Did you know?
+        <div className='subwords'>
+            <b>Did you know?</b>
         </div>
         <div className='words'>
             {fact}
         </div> 
+        <div  className='map'>
+        <MapContainer  center={position} zoom={13} scrollWheelZoom={false} style={{ width: "300px", height: "300px" }} >
+            <Marker position={position}> </Marker>
+            <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+        </MapContainer>
+        </div>
     </div>
 }
 
