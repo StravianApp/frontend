f8d468a93067ae53a276cc8d671ad922e23aea59
Neelu Saraswatibhatla <neelu@sbneelu.com>
2022-02-25 13:02:01 +0000

    Add icons to download page

--- src/pages/download-page/DownloadPage.jsx
+++ src/pages/download-page/DownloadPage.jsx
@@ -3,6 +3,10 @@ import logo from '../../assets/logos/full-text-3.png';
 import logo2 from '../../assets/logos/full-text-1.png';
 import poweredByStravaIcon from '../../assets/powered-by-strava.svg';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import iosShareIcon from './assets/ios-share-icon.svg';
+
 const DownloadPage = () => {
     return (
         <div className="download-main">
@@ -14,12 +18,12 @@ const DownloadPage = () => {
             </div>
             <div className="instructions">
                 <div className="content-box">
-                    <div className="subtitle_">Android(Chrome)</div>
-                    <div className="text_">Tap "<b>Add to Home Screen</b>", or tap the <b>Menu</b> icon and then tap "<b>Install</b>" to install <img className="logo" src={logo2} alt="Stravian" />.</div>
+                    <div className="subtitle_">Android (Chrome)</div>
+                    <div className="text_">Tap the <FontAwesomeIcon icon={faEllipsisV} /> (<b>Menu</b>) icon at the top-right and then tap "<b>Install</b>" to install <img className="logo" src={logo2} alt="Stravian" />.</div>
                 </div>
                 <div className="content-box">
                     <div className="subtitle_">iOS (Safari)</div>
-                    <div className="text_">Tap the <b>Share</b> icon and tap "<b>Add to Home Screen</b>" to install <img className="logo" src={logo2} alt="Stravian" />.</div>
+                    <div className="text_">Tap the <img className="ios-share-icon" src={iosShareIcon} alt="Share" /> (<b>Share</b>) icon and then tap "<b>Add to Home Screen</b>" to install <img className="logo" src={logo2} alt="Stravian" />.</div>
                 </div>
             </div>
             <div className="powered-by-strava"><img src={poweredByStravaIcon} alt="Powered by Strava" /></div>
