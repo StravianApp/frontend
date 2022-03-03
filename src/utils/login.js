b90b472f993903f85ed1fb653529e18bbd268f1a
Neelu Saraswatibhatla <neelu@sbneelu.com>
2022-03-03 12:44:51 +0000

    Flock done

--- src/utils/login.js
+++ src/utils/login.js
@@ -7,12 +7,14 @@ const login = async (loginCode) => {
     // return true;
     const userDetails = await getUserDetails(loginCode);
     if (userDetails == null) return false;
-    const { username, jwt } = userDetails;
+    const { username, jwt, stravaId } = userDetails;
     localStorage.setItem('username', username);
     localStorage.setItem('jwt', jwt);
+    localStorage.setItem('stravaId', stravaId);
     return true;
 }
 
 const getUsername = () => localStorage.getItem('username');
+const getStravaId = () => localStorage.getItem('stravaId');
 
-export { loggedIn, logout, login, getUsername };
+export { loggedIn, logout, login, getUsername, getStravaId };
